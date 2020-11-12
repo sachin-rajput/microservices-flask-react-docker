ff9ef3c2fab78c76b80699e46497b1d571d0ac43
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-11 20:53:52 -0500

    Added Flask and created API folder

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,42 +1,36 @@
 # services/users/project/__init__.py
 
-import os  # new
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy  # new
 
+import os
 
-# instantiate the app
-app = Flask(__name__)
+from flask import Flask  # new
+from flask_sqlalchemy import SQLAlchemy
 
-api = Api(app)
-
-# set config
-app_settings = os.getenv('APP_SETTINGS')  # new
-app.config.from_object(app_settings)      # new
 
 # instantiate the db
-db = SQLAlchemy(app)  # new
-
-# model
-class User(db.Model):  # new
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+db = SQLAlchemy()
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-        'status': 'success',
-        'message': 'pong!'
-    }
+# new
+def create_app(script_info=None):
 
+    # instantiate the app
+    app = Flask(__name__)
 
-api.add_resource(UsersPing, '/users/ping')
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
