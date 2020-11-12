2ad1419f4b890ad0041402ba3a222e10edc5f18c
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-11 19:38:47 -0500

    Added PostgresDB

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,7 +3,8 @@
 import os  # new
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
-import sys
+from flask_sqlalchemy import SQLAlchemy  # new
+
 
 # instantiate the app
 app = Flask(__name__)
@@ -13,6 +14,21 @@ api = Api(app)
 # set config
 app_settings = os.getenv('APP_SETTINGS')  # new
 app.config.from_object(app_settings)      # new
+
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+# model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class UsersPing(Resource):
