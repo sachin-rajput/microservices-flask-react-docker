4d0f3abfd1ebe51c19f1fcb5d4600e90d6e0d810
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-11 20:38:03 -0500

    Added tests to project

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -5,7 +5,8 @@ import os  # new
 class BaseConfig:
     """Base configuration"""
     TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'  # new
 
 
 class DevelopmentConfig(BaseConfig):
