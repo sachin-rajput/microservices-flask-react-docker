59002c99b176f2c00adb12f3b5f2292b166a7acc
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-12 21:58:35 -0500

    Added Flask debug tool

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -9,11 +9,14 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'  # new
+    DEBUG_TB_ENABLED = False              # new
+    DEBUG_TB_INTERCEPT_REDIRECTS = False  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+    DEBUG_TB_ENABLED = True  # new
 
 
 class TestingConfig(BaseConfig):
