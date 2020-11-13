ec206149bb179a08666f3dfa09f72ae569475fb4
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-12 23:06:02 -0500

    Added react forms

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,27 +1,63 @@
-import React, { Component } from 'react';  // new
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import UsersList from "./components/UsersList"
+import React, { Component } from 'react' // new
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import UsersList from './components/UsersList'
+import AddUser from './components/AddUser'
 
 // new
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
-      users: []
-    };
+      users: [],
+      username: '', // new
+      email: '' // new
+    }
+
+    this.addUser = this.addUser.bind(this) // new
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
-    this.getUsers();
-  };
+    this.getUsers()
+  }
+
+  addUser(event) {
+    event.preventDefault()
+    // new
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+    // new
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers() // new
+        this.setState({ username: '', email: '' })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
   // new
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { this.setState({ users: res.data.data.users }); })
-    .catch((err) => { console.log(err); });
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then((res) => {
+        this.setState({ users: res.data.data.users })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  handleChange(event) {
+    const obj = {}
+    obj[event.target.name] = event.target.value
+    this.setState(obj)
   }
 
   render() {
@@ -30,9 +66,19 @@ class App extends Component {
         <div className="container">
           <div className="columns">
             <div className="column is-one-third">
-              <br/>
+              <br />
               <h1 className="title is-1">All Users</h1>
-              <hr/><br/>
+              <hr />
+              <br />
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />{' '}
+              {/* new */}
+              <br />
+              <br /> {/* new */}
               <UsersList users={this.state.users} />
             </div>
           </div>
@@ -40,9 +86,6 @@ class App extends Component {
       </section>
     )
   }
-};
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
