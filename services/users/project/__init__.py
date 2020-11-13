e4d88df2a0ac950f5ec026468a97343310654af2
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-12 22:23:27 -0500

    Added React Client service

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,11 +6,13 @@ import os
 from flask import Flask  # new
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension  # new
+from flask_cors import CORS  # new
 
 
 # instantiate the db
 db = SQLAlchemy()
-toolbar = DebugToolbarExtension()  # new
+toolbar = DebugToolbarExtension()
+cors = CORS()  # new
 
 
 # new
@@ -26,6 +28,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)  # new
+    cors.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
