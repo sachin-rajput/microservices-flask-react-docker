13ecc506321d4bd74d457f672123662e430a25aa
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-14 18:23:51 -0500

    Added Auth routes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -10,6 +10,7 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 
+
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
@@ -37,6 +38,8 @@ def create_app(script_info=None):
     # register blueprints
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
+    from project.api.auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     # shell context for flask cli
     @app.shell_context_processor
