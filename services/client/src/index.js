e74b80035f6963042dfbb94fd47a4819fab71d27
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-14 23:08:16 -0500

    Added React Router

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,91 +1,12 @@
-import React, { Component } from 'react' // new
-import ReactDOM from 'react-dom'
-import axios from 'axios'
-import UsersList from './components/UsersList'
-import AddUser from './components/AddUser'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-// new
-class App extends Component {
-  constructor() {
-    super()
+import App from './App.jsx';
 
-    this.state = {
-      users: [],
-      username: '', // new
-      email: '' // new
-    }
-
-    this.addUser = this.addUser.bind(this) // new
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  componentDidMount() {
-    this.getUsers()
-  }
-
-  addUser(event) {
-    event.preventDefault()
-    // new
-    const data = {
-      username: this.state.username,
-      email: this.state.email
-    }
-    // new
-    axios
-      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-      .then((res) => {
-        this.getUsers() // new
-        this.setState({ username: '', email: '' })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
+ReactDOM.render((
   // new
-  getUsers() {
-    axios
-      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-      .then((res) => {
-        this.setState({ users: res.data.data.users })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  handleChange(event) {
-    const obj = {}
-    obj[event.target.name] = event.target.value
-    this.setState(obj)
-  }
-
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br />
-              <h1 className="title is-1">All Users</h1>
-              <hr />
-              <br />
-              <AddUser
-                username={this.state.username}
-                email={this.state.email}
-                addUser={this.addUser}
-                handleChange={this.handleChange}
-              />{' '}
-              {/* new */}
-              <br />
-              <br /> {/* new */}
-              <UsersList users={this.state.users} />
-            </div>
-          </div>
-        </div>
-      </section>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('root'))
