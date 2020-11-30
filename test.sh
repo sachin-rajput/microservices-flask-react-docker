bb872f43a4f240bbe70900c34300ca29dcb5e7b6
Sachin Rajput <sachin.rajput@hotmail.com>
2020-11-30 00:23:52 -0500

    Fix for Cypress tests 2 with debug: short, added tests

--- test.sh
+++ test.sh
@@ -23,6 +23,8 @@ inspect() {
 # run e2e tests
 docker-compose -f docker-compose-prod.yml up -d --build
 docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+docker-compose -f docker-compose-prod.yml exec users python manage.py seed_db
+docker-compose -f docker-compose-prod.yml exec users python manage.py test
 ./node_modules/.bin/cypress run --config baseUrl=http://localhost
 inspect $? e2e
 docker-compose -f docker-compose-prod.yml down
