9158533b4cc99a945e0737194dae5e5242701f5c
Sachin Rajput <sachin.rajput@hotmail.com>
2020-12-01 18:40:31 -0500

    Added React messaging and updated scripts with cov

--- test.sh
+++ test.sh
@@ -44,7 +44,7 @@ all() {
   inspect $? users
   docker-compose exec users flake8 project
   inspect $? users-lint
-  docker-compose exec client npm test -- --coverage
+  docker-compose exec client npm run coverage
   inspect $? client
   docker-compose down
   e2e
