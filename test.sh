e27b5f1d982a88f77a1309248d16cbb3b2a618ba
Sachin Rajput <sachin.rajput@hotmail.com>
2020-12-02 01:07:17 -0500

    test for e2e

--- test.sh
+++ test.sh
@@ -32,6 +32,7 @@ client() {
 e2e() {
   docker-compose -f docker-compose-prod.yml up -d --build
   docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
+  docker-compose -f docker-compose-prod.yml exec users python manage.py seed_db
   ./node_modules/.bin/cypress run --config baseUrl=http://localhost
   inspect $? e2e
   docker-compose -f docker-compose-prod.yml down
@@ -39,14 +40,14 @@ e2e() {
 
 # run all tests
 all() {
-  docker-compose up -d --build
-  docker-compose exec users python manage.py test
-  inspect $? users
-  docker-compose exec users flake8 project
-  inspect $? users-lint
-  docker-compose exec client npm run coverage
-  inspect $? client
-  docker-compose down
+  # docker-compose up -d --build
+  # docker-compose exec users python manage.py test
+  # inspect $? users
+  # docker-compose exec users flake8 project
+  # inspect $? users-lint
+  # docker-compose exec client npm run coverage
+  # inspect $? client
+  # docker-compose down
   e2e
 }
 
