17ff7aba617ecddb5b6f5edb162cf5714e947f3e
Sachin Rajput <sachin.rajput@hotmail.com>
2020-12-02 19:21:16 -0500

    Added Cache pull for AWS ECR

--- docker-push.sh
+++ docker-push.sh
@@ -27,19 +27,23 @@ then
      [ "$TRAVIS_BRANCH" == "production" ]
   then
     # users
-    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker pull $REPO/$USERS:$TRAVIS_BRANCH
+    docker build --cache-from $REPO/$USERS:$TRAVIS_BRANCH -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
     docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
     docker push $REPO/$USERS:$TAG
     # users db
-    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker pull $REPO/$USERS_DB:$TRAVIS_BRANCH
+    docker build --cache-from $REPO/$USERS_DB:$TRAVIS_BRANCH -t $USERS_DB:$COMMIT -f Dockerfile
     docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
     docker push $REPO/$USERS_DB:$TAG
     # client
-    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD # new
+    docker pull $REPO/$CLIENT:$TRAVIS_BRANCH
+    docker build --cache-from $REPO/$CLIENT:$TRAVIS_BRANCH -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD # new
     docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
     docker push $REPO/$CLIENT:$TAG
     # swagger
-    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
+    docker pull $REPO/$SWAGGER:$TRAVIS_BRANCH
+    docker build --cache-from $REPO/$SWAGGER:$TRAVIS_BRANCH -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV  # new
     docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
     docker push $REPO/$SWAGGER:$TAG
   fi
